604d715c6de11b45731eb8eff81be2ededcf6fed
biancateodoro <biancaurtado645@gmail.com>
2026-02-05 22:16:51 -0300

    feat: create configuration project

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+import process from 'node:process';
+
+export default defineConfig({
+  schema: './src/db/schema.ts',
+  out: './drizzle',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
